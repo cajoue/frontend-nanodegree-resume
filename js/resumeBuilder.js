4ecb43eb0dab7074256ababbd641c67e1640ac34
cajoue <cajoue@users.noreply.github.com>
2015-09-28 20:54:47 +0200

    feat: Add funThoughts [string].replace method

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,15 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-$("#main").append("Valerie Yallup");
+
+// $("#main").append("Valerie Yallup");
+
+var awesomeThoughts = "I am Sasquene and I am AWESOME!";
+
+console.log(awesomeThoughts);
+
+// [string].replace([old],[new]);
+
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+
+$("#main").append(funThoughts);
